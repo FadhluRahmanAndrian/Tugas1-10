fcb50b502cc7bfbe05affa8dbb59d156aae9f2f0
FadhluRahmanAndrian <fadhlurahmanandrian123@gmail.com>
2024-09-26 22:20:43 +0700

    Create latihan4_Fadhlu_rumus_segitiga_samakaki.cpp

---
+++ latihan4_Fadhlu_rumus_segitiga_samakaki.cpp
@@ -0,0 +1,19 @@
+//latihan 4
+//Dibuat oleh Fadhlu Rahman Andrian
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int luas,alas,tinggi;
+	cout<<"masukkan niali alas =  ";
+	cin>>alas;
+	cout<<"masukkan niali tinggi = " ;
+	cin>>tinggi;
+	luas=0.5*alas*tinggi;
+	cout<<"luas segitiga sama kaki  = "<<luas<<endl;
+	return 0;
+	
+}
