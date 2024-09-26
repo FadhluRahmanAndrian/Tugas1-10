c2b0aed145882baabb0d78b2cdc7ef0e9bf4c82b
FadhluRahmanAndrian <fadhlurahmanandrian123@gmail.com>
2024-09-26 22:28:12 +0700

    Create latihan8_Fadhlu_pengguanaan_case.cpp

---
+++ latihan8_Fadhlu_pengguanaan_case.cpp
@@ -0,0 +1,27 @@
+//latihan 8
+//Dibuat oleh Fadhlu Rahman Andrian
+
+#include <iomanip>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int bil;
+	
+	cout<<"Ketika Angka Dari [0..9] = ";
+	cin>>bil;
+	switch(bil){
+		case 0:
+				cout<<"Anda Mengetikan Angka 0";
+				break;
+		case 1:
+				cout<<"Anda Mengetikan Angka 1";
+				break;
+		default:
+				cout<<"Anda Mengetikan Yang Salah";
+						
+	}
+	return 0;
+}
