5226c83c161f329d29c9691ce499b5a9427ec45d
FadhluRahmanAndrian <fadhlurahmanandrian123@gmail.com>
2024-09-26 22:17:46 +0700

    Create latihan3_Fadhlu_rumus_menggunakan_keyboard.cpp

---
+++ latihan3_Fadhlu_rumus_menggunakan_keyboard.cpp
@@ -0,0 +1,19 @@
+//latihan 2
+//Dibuat oleh Fadhlu Rahman Andrian
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int luas,panjang,lebar;
+	cout<<"masukkan niali panjang =  ";
+	cin>>panjang;
+	cout<<"masukkan niali lebar = " ;
+	cin>>lebar;
+	luas=panjang*lebar;
+	cout<<"luas persegipanjang = "<<luas<<endl;
+	return 0;
+	
+}
