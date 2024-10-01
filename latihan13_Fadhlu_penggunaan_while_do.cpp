6c0c6b5c85e5e4014f452f87c08641d2e6b79145
FadhluRahmanAndrian <fadhlurahmanandrian123@gmail.com>
2024-10-02 06:20:42 +0700

    Create latihan13_Fadhlu_penggunaan_while_do.cpp

---
+++ latihan13_Fadhlu_penggunaan_while_do.cpp
@@ -0,0 +1,18 @@
+//latihan 13
+//Dibuat oleh Fadhlu Rahman Andrian
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main(){
+	int i =0,k=0;
+	
+	do {
+		cout <<"perulanagn ke-"<<i<<","<<k<<endl;
+		i++,k++;
+	}while(i<5);
+	while(k<5);
+	return 0;
+}
