5161984139052aaf21f67907cc5b0397562d4bc1
FadhluRahmanAndrian <fadhlurahmanandrian123@gmail.com>
2024-10-02 06:22:22 +0700

    Create latihan14_Fadhlu_penjualan_menggunakan_for.cpp

---
+++ latihan14_Fadhlu_penjualan_menggunakan_for.cpp
@@ -0,0 +1,23 @@
+//latihan 14
+//Dibuat oleh Fadhlu Rahman Andrian
+
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+int main (){
+	int jlhdata,jlhbarng,ttlpenjualan=0;
+	
+	cout<<"Masukkan Jumlah Data";
+	cin>>jlhdata;
+	
+	for (int i= 1;i<=jlhdata;i++){
+		cout<<"Masukkan Jumlah Barang Yang Terjual Di Toko "<<i<<": ";
+		cin>>jlhbarng;
+		
+		ttlpenjualan += jlhbarng;//<--rumus disingkat dari(ttlpenjualan=ttlpenjualan+jlhbarng)
+	}
+	cout<<"Total Barang Yang Terjual Di Toko Adalah : "<<ttlpenjualan <<endl;
+	return 0;
+}
